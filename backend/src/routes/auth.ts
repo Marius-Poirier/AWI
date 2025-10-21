fc7435f051a6d1b37f597a83a592f346b5975505
ASAP Marius <marius.poirierpro@gmail.com>
2025-10-21 20:47:44 +0200

    version sans le tsconfig.build et les extenseions ts => js.
    Erreur CORS (??)

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -2,8 +2,8 @@ import { Router } from 'express'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 import pool from '../db/database.js'
-import { verifyToken, createAccessToken, createRefreshToken } from '../middleware/token-management.ts';
-import type { TokenPayload } from '../types/token-payload.ts';
+import { verifyToken, createAccessToken, createRefreshToken } from '../middleware/token-management.js';
+import type { TokenPayload } from '../types/token-payload.js';
 
 const router = Router()
 router.post('/login', async (req, res) => { // --- LOGIN ---
